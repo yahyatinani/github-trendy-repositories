377da78b477d950e67623d237a3b14b9043f6306
Yahya <yahyatinani@gmail.com>
2021-01-25 12:52:40 +0100

    Update the color palette

--- android/src/main/java/com/why/githubtrendyrepos/theme/Colors.kt
+++ android/src/main/java/com/why/githubtrendyrepos/theme/Colors.kt
@@ -2,9 +2,9 @@ package com.why.githubtrendyrepos.theme
 
 import androidx.compose.ui.graphics.Color
 
-// TODO: Make your own colors
+val Blue300 = Color(0xff54c6f0)
+val Blue600 = Color(0xff019FDE)
+val Blue900 = Color(0xff005b96)
+
 val Red200 = Color(0xfff297a2)
-val Red300 = Color(0xffea6d7e)
-val Red700 = Color(0xffdd0d3c)
 val Red800 = Color(0xffd00036)
-val Red900 = Color(0xffc20029)
