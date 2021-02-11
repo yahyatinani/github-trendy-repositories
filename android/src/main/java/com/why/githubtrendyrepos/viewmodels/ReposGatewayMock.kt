ff1580f25b5de31d4b9d1f2597145b8a46d0f82c
Yahya <yahyatinani@gmail.com>
2021-02-11 13:19:03 +0100

    Implement GetTrendyReposUseCase

--- android/src/main/java/com/why/githubtrendyrepos/viewmodels/ReposGatewayMock.kt
+++ android/src/main/java/com/why/githubtrendyrepos/viewmodels/ReposGatewayMock.kt
@@ -3,23 +3,36 @@ package com.why.githubtrendyrepos.viewmodels
 import com.why.githubtrendyrepos.app.Repo
 import com.why.githubtrendyrepos.app.ReposGateway
 import com.why.githubtrendyrepos.app.Result
-import com.why.githubtrendyrepos.app.Result.Ok
+import kotlinx.datetime.Clock
+import kotlinx.datetime.DatePeriod
 import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.minus
+import kotlinx.datetime.toLocalDateTime
 
-class ReposGatewayMock : ReposGateway {
+class ReposGatewayMock() : ReposGateway {
+
+    val repo: Repo = Repo(
+        name = "repo-example",
+        description = "cool repo, try it out!!",
+        starsCount = 185,
+        author = "author",
+        avatarUrl = ""
+    )
+
     override suspend fun getMostStaredReposSince(
         creationDate: LocalDate, page: Int
-    ): Map<Result, Any> = mapOf(
-        Ok to listOf(
-            Repo(
-                name = "repo-example",
-                description = "cool repo, try it out!!",
-                starsCount = 185,
-                author = "author",
-                avatarUrl = ""
-            )
-        )
-    )
+    ): Map<Result, Any> {
+        return when {
+            page == 1 || creationDate == creationDate() ->
+                mapOf(Result.Ok to listOf(repo))
+            else -> mapOf()
+        }
+    }
+
+    private fun creationDate() = Clock.System.now()
+        .toLocalDateTime(TimeZone.currentSystemDefault())
+        .date.minus(DatePeriod(days = 30))
 
     override fun close() {}
 }
